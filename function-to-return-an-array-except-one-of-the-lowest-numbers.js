7e1dfb6401294b02156e561692d9557750bcd98a
Ryan Essel <spucti19@gmail.com>
2023-04-21 22:56:25 -0400

    js tricks update

--- function-to-return-an-array-except-one-of-the-lowest-numbers.js
+++ function-to-return-an-array-except-one-of-the-lowest-numbers.js
@@ -1,5 +1,6 @@
 // this is useful for getting dnd stats when you make a character
-
+//this will remove only 1 instance of the lowest number but leave the remainder.
+// again the bit below makes sene for dnd
 function removeLowestValeOnce(arr){
 
     for (let i = 0; i < arr.length; i++){
